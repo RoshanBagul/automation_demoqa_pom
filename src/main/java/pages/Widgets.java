2ccc356c5dddcf08213bfb2ccfe80f76b6a51c92
Roshan Bagul <roshanbagul1904@gmail.com>
2026-01-23 18:07:11 +0530

    37 Automation For Progress Bar

--- src/main/java/pages/Widgets.java
+++ src/main/java/pages/Widgets.java
@@ -1,4 +1,29 @@
 package pages;
 
-public class Widgets {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import pages.*;
+import base.BasePage;
+
+public class Widgets extends BasePage{
+
+    public Widgets(WebDriver driver){
+        this.driver = driver;
+    }
+   
+    public By ProgressBarTitle = By.xpath("//h1[text()='Progress Bar']");
+    public By progress_bar = By.id("progressBar");
+    public By ProgressBarCollaps = By.xpath("//div[@class='element-list collapse show']//li[@id='item-4']");
+    public By startStopButton = By.id("startStopButton");
+    public By fifty_Percent = By.xpath("//div[@id='progressBar']/div[text()='51%']");
+
+    public void openProgressBar(){
+        driver.findElement(ProgressBarCollaps).click();
+    }
+
+    public void clickStartStopButton(){
+        driver.findElement(startStopButton).click();
+    }
+
+
 }
